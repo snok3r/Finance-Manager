4b87cf875d7e04c232e998306715e753944f0c19
snok3r <kos.davidenko@gmail.com>
2016-04-04 21:21:56 +0300

    - started Account class

--- src/Account.java
+++ src/Account.java
@@ -1,3 +1,38 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class Account {
+
+    private String description;
+    private float balance;
+    private Set<Record> records;
+
+    public Account(String description) {
+        this.description = description;
+        this.balance = 0.0f;
+        this.records = new LinkedHashSet<>();
+    }
+
+    public Account(String description, float balance) {
+        this(description);
+        this.balance = balance;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Set<Record> getRecords() {
+        return new LinkedHashSet<>(records);
+    }
+
+    public static void main(String[] args) {
+        Account account = new Account("Test account");
+        Set<Record> records = account.getRecords();
+        records.add(new Record());
+    }
 }

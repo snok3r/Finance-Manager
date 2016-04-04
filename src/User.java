cdaeb825a8dd881d4223a9f9578f127c6ef46d28
snok3r <kos.davidenko@gmail.com>
2016-04-04 20:54:00 +0300

    - now user can change his/her password

--- src/User.java
+++ src/User.java
@@ -2,10 +2,25 @@
 public class User {
 
     private final String login;
-    private final String password;
+    private String password;
 
     public User(String login, String password) {
         this.login = login;
+        setPassword(password);
+    }
+
+    public void changePassword(String oldPassword, String newPassword) {
+        if (!checkPassword(oldPassword))
+            throw new IllegalArgumentException("passwords don't match or new password is < 5 or >= 15 characters long");
+
+        try {
+            setPassword(newPassword);
+        } catch (IllegalArgumentException e) { // not letting client know, that oldPassword is right
+            throw new IllegalArgumentException("passwords don't match or new password is < 5 or >= 15 characters long");
+        }
+    }
+
+    private void setPassword(String password) {
         if (password.length() >= 5 && password.length() < 15)
             this.password = saltingPassword(password);
         else
@@ -44,5 +59,8 @@ public class User {
         System.out.println(user.checkPassword(user.saltingPassword(user.password)));
         System.out.println(user.checkPassword("koasas"));
         System.out.println(user.checkPassword("kos"));
+
+        user.changePassword("kostya", "kostya123");
+        System.out.println(user.saltingPassword(user.password));
     }
 }
